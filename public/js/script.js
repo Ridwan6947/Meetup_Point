10b9c9ba7ce7210101dfe290d7fca8db9b1dc457
Ridwan6947 <ridwanahamed47@gmail.com>
2024-12-11 22:51:50 +0530

    added polylines to connect to users with a black line

--- public/js/script.js
+++ public/js/script.js
@@ -24,17 +24,46 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const markers = {};
 
-socker.on("receive-location" , (data)=>{
-    const {id , latitude , longitude} = data;
-    map.setView([latitude , longitude]);
-    if(markers[id]){
-        markers[id].setLatLng([latitude , longitude]);
-    }else{
-        markers[id] = L.marker([latitude , longitude]).addTo(map);
-    }
-})
+socker.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    userLocations[id] = { latitude, longitude };
 
-socker.on("user-disconnected" , (id)=>{
+    // Create or update the marker for the user
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+
+    // If two users exist, draw a line between them and find the meeting point
+    const userIds = Object.keys(userLocations); // Get the user IDs
+    if (userIds.length === 2) {
+        const user1 = userLocations[userIds[0]]; // Get the locations of the two users
+        const user2 = userLocations[userIds[1]];
+        const latLngs = [                           // Create the polyline coordinates
+            [user1.latitude, user1.longitude],
+            [user2.latitude, user2.longitude]
+        ];
+
+        if (polylines[userIds.join("-")]) {
+            polylines[userIds.join("-")].setLatLngs(latLngs); // Update the polyline
+        } else {
+            polylines[userIds.join("-")] = L.polyline(latLngs, { color: 'black' }).addTo(map); // Draw a new polyline
+        }
+
+        // // Find and add the meeting point
+        // const meetingPoint = findClosestMeetingPoint(userLocations);
+        // if (meetingPoint) {
+        //     L.marker([meetingPoint.lat, meetingPoint.lon])
+        //         .addTo(map)
+        //         .bindPopup(meetingPoint.name)
+        //         .openPopup();
+        // }
+    }
+});
+
+
+socker.on("user-disconnected" , (id)=>{  // delete user when app closes
     if(markers[id]){
         map.removeLayer(markers[id]);
         delete markers[id];
